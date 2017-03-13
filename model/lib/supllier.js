9d73ef63b65581e2e3648646da2861745f05344c
root <root@localhost>
2017-03-13 19:09:50 +0800

    fix

--- model/lib/supllier.js
+++ model/lib/supllier.js
@@ -2,23 +2,23 @@
 import _ from 'underscore'
 import * as Base from './_base'
 
-// 库房 也可以是展厅
-
+// 供货商  代替原来的品牌 品牌意思太单薄了
 export var keys = Object.assign({},Base.keys,{
 
-        name: {
-            type: String,
-            default: ''
-        },
-        city: {
+        customer_num: {
             type: String,
             default: ''
         },
         note: {
             type: String,
             default: ''
+        },
+        
+        contract_num: {
+            type: String,
+            default: ''
         }
 
     })
-export var PRE = 'WH'
+export var PRE = 'RC'
 Base._getThis(exports,keys,__filename)
