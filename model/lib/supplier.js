a8a11b877e8f4d0cebe5a54c86ebd0d24a04367c
张利涛 <i@tony-zh.com>
2017-03-14 19:45:35 +0800

    fx

--- model/lib/supplier.js
+++ model/lib/supplier.js
@@ -5,16 +5,15 @@ import * as Base from './_base'
 // 供货商  代替原来的品牌 品牌意思太单薄了
 export var keys = Object.assign({},Base.keys,{
 
-        customer_num: {
+        name: { //名字 如 品牌名
+            type: String,
+            default: ''
+        },
+        country: { //国家
             type: String,
             default: ''
         },
         note: {
-            type: String,
-            default: ''
-        },
-
-        contract_num: {
             type: String,
             default: ''
         }
