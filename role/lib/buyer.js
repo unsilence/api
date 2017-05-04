ec131cd12e1790da0236e7dec1420f89cdfa6bd7
tony <i@tony-zh.com>
2017-05-04 23:18:42 +0800

    差不多采购权限 封装了 ：】『

--- role/lib/buyer.js
+++ role/lib/buyer.js
@@ -1,27 +1,35 @@
 "use strict"
 var _ = require( 'underscore');
 
+const _check = user=>{
+  let brands = JSON.parse(user.role_data||'{"test":1}')
+  return [true,{supplier_num:{$in:brands}}]
+}
+const _add_check = user=>{
+  let brands = JSON.parse(user.role_data||'{"test":1}')
+  return [true,item=>item.supplier_num in brands]
+}
 const collections = {
   // 需要品牌权限过滤
   Component: {
-    getById: user =>[true, {}],
-    getByNum: user =>[true, {}],
-    deleteById: user =>[true, {}],
-    deleteByNum: user =>[true, {}],
-    updateById: user =>[true, {}],
-    updateByNum: user =>[true, {}],
-    fetch: user =>[true, {}],
-    addItem: user =>[true, {}],
+    getById: _check,
+    getByNum: _check,
+    deleteById: _check,
+    deleteByNum: _check,
+    updateById: _check,
+    updateByNum: _check,
+    fetch: _check,
+    addItem: _add_check,
   },
   Product: {
-    getById: user =>[true, {}],
-    getByNum: user =>[true, {}],
-    deleteById: user =>[true, {}],
-    deleteByNum: user =>[true, {}],
-    updateById: user =>[true, {}],
-    updateByNum: user =>[true, {}],
-    fetch: user =>[true, {}],
-    addItem: user =>[true, {}],
+    getById: _check,
+    getByNum: _check,
+    deleteById: _check,
+    deleteByNum: _check,
+    updateById: _check,
+    updateByNum: _check,
+    fetch: _check,
+    addItem: _add_check,
   },
   Proposal: {
     getById: user =>[true, {}],
@@ -34,14 +42,14 @@ const collections = {
     addItem: user =>[true, {}],
   },
   Purchase: {
-    getById: user =>[true, {}],
-    getByNum: user =>[true, {}],
-    deleteById: user =>[true, {}],
-    deleteByNum: user =>[true, {}],
-    updateById: user =>[true, {}],
-    updateByNum: user =>[true, {}],
-    fetch: user =>[true, {}],
-    addItem: user =>[true, {}],
+    getById: _check,
+    getByNum: _check,
+    deleteById: _check,
+    deleteByNum: _check,
+    updateById: _check,
+    updateByNum: _check,
+    fetch: _check,
+    addItem: _add_check,
   },
   // 只能获取
   Customer: {
@@ -106,18 +114,24 @@ const collections = {
   },
   // 仅能获取和个人帐号相关的
   Message: {
-    getById: user =>[true, {}],
-    getByNum: user =>[true, {}],
-    fetch: user =>[true, {}],
+    getById: user =>[true, {"$or":[{user_num:user.cnum},{to_user:user.cnum}]}],
+    getByNum: user =>[true, {"$or":[{user_num:user.cnum},{to_user:user.cnum}]}],
+    fetch: user =>[true, {"$or":[{user_num:user.cnum},{to_user:user.cnum}]}],
     addItem: user =>[true, {}],
   },
   Remark: {
-    getById: user =>[true, {}],
-    getByNum: user =>[true, {}],
-    fetch: user =>[true, {}],
-    addItem: user =>[true, {}],
+    getById: user =>[true, {user_num:user.cnum}],
+    getByNum: user =>[true, {user_num:user.cnum}],
+    fetch: user =>[true,{user_num:user.cnum}],
+    addItem: user =>[true,{user_num:user.cnum}],
   },
-
+  User:{
+    getById: user =>[true, {cnum:user.cnum}],
+    getByNum: user =>[true, {cnum:user.cnum}],
+    updateById: user =>[true, {cnum:user.cnum}],
+    updateByNum: user =>[true, {cnum:user.cnum}],
+    fetch: user =>[true, {cnum:user.cnum}],
+  }
 }
 
 var check = exports.check = async(colName, action, currentUser) => {
